132d2421514378978ebf23e245b0bcf7604a5131
tomasbasso <tomas.basso@hotmail.com>
2024-08-21 16:14:38 -0300

    Diseño final de login

--- AppFakeStore/MauiProgram.cs
+++ AppFakeStore/MauiProgram.cs
@@ -2,6 +2,7 @@
 using AppFakeStore.ViewModels;
 using AppFakeStore.Views;
 using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace AppFakeStore
 {
@@ -12,11 +13,15 @@ namespace AppFakeStore
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseSkiaSharp()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialDesignIcons");
+                    fonts.AddFont("Lato-Bold.ttf", "bold");
+                    fonts.AddFont("Lato-Regular.ttf", "medium");
+                    fonts.AddFont("Solid.otf", "AwesomeSolid");
                 });
 
             builder.Services.AddSingleton<IProductoService, ProductoService>();
